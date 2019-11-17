62fa00fe2f722342e20899571aec8a3d36d36e1b
Micah Zaayer <micahzaayer@gmail.com>
2019-11-17 13:06:20 -0800

    add state and lifting state to project.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+  handleAddingNewTicketToList(newTicket) {
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({ masterTicketList: newMasterTicketList });
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={() => <TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={() => <NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header(){
     <div>
       <h1>Help Queue</h1>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
-      </div>
+    </div>
   );
 }
 
